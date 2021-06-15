74b18774ecc97a2216373cc9328c11b886df3ea9
Francesca Salvini <franci98sp@gmail.com>
2021-06-15 16:30:51 +0200

    inserita card comic ( con bootstrap )

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>Comics</h1>
+    <section>
+        <div class="container">
+            <h1>Comics</h1>
+
+            <div class="row">
+
+                @foreach($comics as $comic)
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ $comic->image }}" alt="">
+                        <div class="card-body">
+                          <h5 class="card-title">{{ $comic->name }}</h5>
+
+                          @if($comic->description)
+                            <p class="card-text">{{ $comic->description }}</p>
+                          @endif
+
+                          <a href="{{ route('comics.show', [
+                              'comic' => $comic->id
+                          ]) }}" class="btn btn-primary">
+                            Guarda i dettagli
+                        </a>
+                        </div>
+                      </div>
+                </div>
+                @endforeach
+                
+            </div>
+        </div>
+    </section>
 @endsection
